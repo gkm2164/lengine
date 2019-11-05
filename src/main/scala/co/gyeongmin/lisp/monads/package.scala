6a45c6cd092ed5bf09a086096319f0c879e5ff4e
Gyeongmin Go <gkm2164@gmail.com>
2019-11-06 01:11:51 +0900

    add char type

--- src/main/scala/co/gyeongmin/lisp/monads/package.scala
+++ src/main/scala/co/gyeongmin/lisp/monads/package.scala
@@ -6,25 +6,6 @@ import co.gyeongmin.lisp.execution.LispEnvironment
 import co.gyeongmin.lisp.lexer._
 
 package object monads {
-  type LispState[A] = (Stream[LispToken], LispEnvironment) => Either[LispError, (A, Stream[LispToken], LispEnvironment)]
-
-  implicit val lispStateMonad: Monad[LispState] = new Monad[LispState] {
-    override def pure[A](x: A): LispState[A] = (tokens, env) => Right((x, tokens, env))
-
-    override def flatMap[A, B](fa: LispState[A])(f: A => LispState[B]): LispState[B] = (tokens, env) => {
-      fa(tokens, env) match {
-        case Right((a, nextTokens, nextEnv)) => f(a)(nextTokens, nextEnv)
-        case Left(e) => Left(e)
-      }
-    }
-
-    override def tailRecM[A, B](a: A)(f: A => LispState[Either[A, B]]): LispState[B] = (tokens, env) => f(a)(tokens, env) match {
-      case Left(e) => Left(e)
-      case Right((Left(la), nextTokens, nextEnv)) => tailRecM(la)(f)(nextTokens, nextEnv)
-      case Right((Right(rb), nextTokens, nextEnv)) => Right((rb, nextTokens, nextEnv))
-    }
-  }
-
   type LispTokenState[A] = Stream[LispToken] => Either[ParseError, (A, Stream[LispToken])]
 
   implicit val lispTokenStateMonad: Monad[LispTokenState] = new Monad[LispTokenState] {
