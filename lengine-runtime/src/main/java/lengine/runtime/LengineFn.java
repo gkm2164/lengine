275ccca4d82024735454a935dff1492b8b735846
Gyeongmin Go <gkm2164@gmail.com>
2023-01-28 13:30:34 -0800

    refine LengineFn type to handle overloaded methods

--- lengine-runtime/src/main/java/lengine/runtime/LengineFn.java
+++ lengine-runtime/src/main/java/lengine/runtime/LengineFn.java
@@ -2,9 +2,19 @@ package lengine.runtime;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class LengineFn {
+  private static final Set<String> initialized = new HashSet<>();
+  private static final Map<String, ArrayList<Method>> methodMap = new HashMap<>();
+
   private final String name;
   private final String[] args;
   private final Method method;
@@ -18,14 +28,43 @@ public class LengineFn {
   }
 
   public static LengineFn create(String clsName, String name, String[] args, Object[] captured) {
-    try {
-      Class[] clsArr = new Class[args.length + captured.length];
-      Arrays.fill(clsArr, Object.class);
-      Method method = Class.forName(clsName).getMethod(name, clsArr);
-      return new LengineFn(name, args, method, captured);
-    } catch (ClassNotFoundException | NoSuchMethodException e) {
-      throw new RuntimeException(e);
+    if (!initialized.contains(clsName)) {
+      initMethodCache(clsName);
     }
+
+    Class[] clsArr = new Class[args.length + captured.length];
+    Arrays.fill(clsArr, Object.class);
+
+    if (!methodMap.containsKey(name)) {
+        throw new RuntimeException("no such method defined!");
+      }
+
+    List<Method> matchedMethods = methodMap.get(name)
+          .stream()
+          .filter(x -> x.getParameters().length == clsArr.length)
+          .collect(Collectors.toList());
+
+    if (matchedMethods.isEmpty()) {
+        throw new RuntimeException("unable to find method with signature");
+      }
+    if (matchedMethods.size() > 1) {
+        throw new RuntimeException("more than one method identified for the given");
+      }
+
+    Method method = matchedMethods.stream().findFirst().get();
+    return new LengineFn(name, args, method, captured);
+  }
+
+  private static void initMethodCache(String className) {
+    try {
+      for (Method method : Class.forName(className).getMethods()) {
+        List<Method> arrayList = methodMap.computeIfAbsent(method.getName(), (key) -> new ArrayList<>());
+        arrayList.add(method);
+      }
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException("Failed to initialize!");
+    }
+    initialized.add(className);
   }
 
   public Object invoke(Object... args) {
