62e8f394be3e3008974306b46e514580ca49fe32
Gyeongmin Go <gkm2164@gmail.com>
2023-02-08 00:16:53 -0800

    add error handling

--- lengine-runtime/src/main/java/lengine/https/HandlerWrapper.java
+++ lengine-runtime/src/main/java/lengine/https/HandlerWrapper.java
@@ -3,6 +3,7 @@ package lengine.https;
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+
 import lengine.functions.LengineLambda1;
 import lengine.functions.LengineLambda2;
 import lengine.runtime.LengineUnit;
@@ -18,55 +19,61 @@ import java.io.PrintStream;
 import static lengine.https.HttpServerBuilder.UNSAFE_cast;
 
 public class HandlerWrapper implements HttpHandler {
-    private final LengineMap lambdaMethodMapping;
+  private final LengineMap lambdaMethodMapping;
 
-    public HandlerWrapper(LengineMap lambdaMethodMapping) {
-        this.lambdaMethodMapping = lambdaMethodMapping;
+  public HandlerWrapper(LengineMap lambdaMethodMapping) {
+    this.lambdaMethodMapping = lambdaMethodMapping;
+  }
+
+  @Override
+  public void handle(HttpExchange t) throws IOException {
+    try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream(); OutputStream os = t.getResponseBody()) {
+      ResponseBuilder responseBuilder = new ResponseBuilder(new PrintStream(outputStream));
+      RequestBuilder requestBuilder = new RequestBuilder(t);
+      String method = t.getRequestMethod();
+
+      final LengineLambda2<LengineUnit, LengineMap, LengineMap> handler;
+
+      if (this.lambdaMethodMapping.contains(LengineMapKey.create("ALL"))) {
+        handler = UNSAFE_cast(this.lambdaMethodMapping.get(LengineMapKey.create("ALL")));
+      } else if (this.lambdaMethodMapping.contains(LengineMapKey.create(method))) {
+        handler = UNSAFE_cast(this.lambdaMethodMapping.get(LengineMapKey.create(method)));
+      } else {
+        handler = this::nullHandler;
+      }
+
+      handler.invoke(requestBuilder.build(), responseBuilder.build());
+
+      t.sendResponseHeaders(responseBuilder.getStatusCode(), outputStream.size());
+
+      final Headers responseHeaders = t.getResponseHeaders();
+      responseBuilder.getHeaders().entries().iterator().forEachRemaining(_entry -> {
+        LengineMapEntry entry = (LengineMapEntry) _entry;
+        String header = entry.getKey().getKey();
+        String value = (String) entry.getValue();
+
+        responseHeaders.add(header, value);
+      });
+      os.write(outputStream.toByteArray());
+    } catch (Exception e) {
+      e.printStackTrace();
+      String message = "Internal server error";
+      t.sendResponseHeaders(500, message.length());
+      OutputStream os = t.getResponseBody();
+      os.write(message.getBytes());
+      os.close();
     }
+  }
 
-    @Override
-    public void handle(HttpExchange t) throws IOException {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        ResponseBuilder responseBuilder = new ResponseBuilder(new PrintStream(outputStream));
-        RequestBuilder requestBuilder = new RequestBuilder(t);
-        String method = t.getRequestMethod();
+  private LengineUnit nullHandler(LengineMap req, LengineMap res) {
+    LengineLambda1<LengineUnit, Long> setStatusCode = UNSAFE_cast(res.get(LengineMapKey.create("set-status-code")));
+    setStatusCode.invoke(404L);
+    LengineLambda1<LengineUnit, LengineMap> setHeaders = UNSAFE_cast(res.get(LengineMapKey.create("set-headers")));
+    setHeaders.invoke(LengineMap.create()
+        .putEntry(LengineMapEntry.create(LengineMapKey.create("Content-Type"), "text/html")));
 
-        final LengineLambda2<LengineUnit, LengineMap, LengineMap> handler;
-
-        if (this.lambdaMethodMapping.contains(LengineMapKey.create("ALL"))) {
-            handler = UNSAFE_cast(this.lambdaMethodMapping.get(LengineMapKey.create("ALL")));
-        } else if (this.lambdaMethodMapping.contains(LengineMapKey.create(method))) {
-            handler = UNSAFE_cast(this.lambdaMethodMapping.get(LengineMapKey.create(method)));
-        } else {
-            handler = this::nullHandler;
-        }
-
-        handler.invoke(requestBuilder.build(), responseBuilder.build());
-
-        t.sendResponseHeaders(responseBuilder.getStatusCode(), outputStream.size());
-
-        final Headers responseHeaders = t.getResponseHeaders();
-        responseBuilder.getHeaders().entries().iterator().forEachRemaining(_entry -> {
-            LengineMapEntry entry = (LengineMapEntry)_entry;
-            String header = entry.getKey().getKey();
-            String value = (String)entry.getValue();
-
-            responseHeaders.add(header, value);
-        });
-        OutputStream os = t.getResponseBody();
-        os.write(outputStream.toByteArray());
-        os.close();
-    }
-
-    private LengineUnit nullHandler(LengineMap req, LengineMap res) {
-        LengineLambda1<LengineUnit, Long> setStatusCode = UNSAFE_cast(res.get(LengineMapKey.create("set-status-code")));
-        setStatusCode.invoke(404L);
-        LengineLambda1<LengineUnit, LengineMap> setHeaders = UNSAFE_cast(res.get(LengineMapKey.create("set-headers")));
-        setHeaders.invoke(LengineMap.create()
-                .putEntry(LengineMapEntry.create(LengineMapKey.create("Content-Type"), "text/html")));
-
-        LengineLambda1<LengineUnit, String> writer = UNSAFE_cast(res.get(LengineMapKey.create("writer")));
-        writer.invoke("<h1>No handler setup</h1>");
-        return LengineUnit.create();
-    }
+    LengineLambda1<LengineUnit, String> writer = UNSAFE_cast(res.get(LengineMapKey.create("writer")));
+    writer.invoke("<h1>No handler setup</h1>");
+    return LengineUnit.create();
+  }
 }
--- lengine-runtime/src/main/java/lengine/https/HttpServerBuilder.java
+++ lengine-runtime/src/main/java/lengine/https/HttpServerBuilder.java
@@ -1,6 +1,9 @@
 package lengine.https;
 
+import static java.lang.String.format;
+
 import com.sun.net.httpserver.HttpServer;
+
 import lengine.functions.LengineLambda0;
 import lengine.runtime.LengineUnit;
 import lengine.util.LengineMap;
@@ -12,34 +15,36 @@ import java.net.InetSocketAddress;
 import java.util.logging.Logger;
 
 public class HttpServerBuilder {
-    public static LengineLambda0<LengineUnit> listen(LengineMap obj) {
-        String address = (String) obj.get(LengineMapKey.create("host"));
-        Long port = (Long) obj.get(LengineMapKey.create("port"));
-        LengineMap handlers = (LengineMap) obj.get(LengineMapKey.create("handlers"));
+  private static final Logger LOG = Logger.getLogger(HttpServerBuilder.class.getName());
 
-        try {
-            HttpServer server = HttpServer.create(new InetSocketAddress(address, port.intValue()), 0);
-            handlers.entries().iterator().forEachRemaining(_entry -> {
-                final LengineMapEntry entry = (LengineMapEntry) _entry;
-                final LengineMap handler = UNSAFE_cast(entry.getValue());
-                Logger.getLogger(HttpServerBuilder.class.getName())
-                        .info(String.format("created context on %s", entry.getKey().getKey()));
-                server.createContext(entry.getKey().getKey(), new HandlerWrapper(handler));
-            });
-            server.setExecutor(null);
-            server.start();
+  public static LengineLambda0<LengineUnit> listen(LengineMap obj) {
+    String address = (String) obj.get(LengineMapKey.create("host"));
+    Long port = (Long) obj.get(LengineMapKey.create("port"));
+    LengineMap handlers = (LengineMap) obj.get(LengineMapKey.create("handlers"));
 
-            return () -> {
-                server.stop(0);
-                return LengineUnit.create();
-            };
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
+    try {
+      HttpServer server = HttpServer.create(new InetSocketAddress(address, port.intValue()), 0);
+      handlers.entries().iterator().forEachRemaining(_entry -> {
+        final LengineMapEntry entry = (LengineMapEntry) _entry;
+        final LengineMap handler = UNSAFE_cast(entry.getValue());
+        LOG.info(format("created context on %s", entry.getKey().getKey()));
+        server.createContext(entry.getKey().getKey(), new HandlerWrapper(handler));
+      });
+      server.setExecutor(null);
+      server.start();
+
+      LOG.info(format("Server has been started on %s:%d", address, port));
+      return () -> {
+        server.stop(0);
+        return LengineUnit.create();
+      };
+    } catch (IOException e) {
+      e.printStackTrace();
+      throw new RuntimeException(e);
     }
+  }
 
-    static <T> T UNSAFE_cast(Object object) {
-        return (T)object;
-    }
+  static <T> T UNSAFE_cast(Object object) {
+    return (T) object;
+  }
 }
