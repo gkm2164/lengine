feaf583fab7e2b0a03a977a686936f0431c30ad1
Gyeongmin Go <gkm2164@gmail.com>
2023-02-09 23:12:22 -0800

    improve the error message

--- lengine-core/src/main/scala/co/gyeongmin/lisp/errors/parser/UnexpectedTokenError.scala
+++ lengine-core/src/main/scala/co/gyeongmin/lisp/errors/parser/UnexpectedTokenError.scala
@@ -1,9 +1,13 @@
 package co.gyeongmin.lisp.errors.parser
 
-import co.gyeongmin.lisp.lexer.TokenLocation
 import co.gyeongmin.lisp.lexer.tokens.LispToken
 
 case class UnexpectedTokenError(tk: LispToken, msg: String = "")
     extends ParseError {
-  override def message: String = s"unexpected token: '${tk.toString}'(line: ${tk.line}, column:${tk.column})"
+
+  private def trails: Option[String] = for {
+    line <- tk.line
+    column <- tk.column
+  } yield s"(line: $line, column: $column)"
+  override def message: String = s"unexpected token: '${tk.toString}'${trails.getOrElse("")}"
 }
