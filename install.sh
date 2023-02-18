6fab66327d16b52e7332e75869d9cfbb1da188ca
Gyeongmin Go <gyeongm@amazon.com>
2023-02-17 18:36:56 -0800

    try to implement standard library

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ sbt assembly
 cp ./lengine-repl/target/scala-2.12/lengine.jar .
 cp ./lengine-compiler/target/scala-2.12/lenginec.jar .
 cp ./lengine-runtime/target/scala-2.12/lengine-runtime.jar .
+
+# Building some standard library
+./lenginec lengine-code/stdlib.lg
