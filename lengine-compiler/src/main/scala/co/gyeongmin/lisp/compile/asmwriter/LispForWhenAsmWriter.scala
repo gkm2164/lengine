79b30720776b4dc25578b30bc45245639f1daf19
Gyeongmin Go <gyeongm@amazon.com>
2023-02-20 11:03:26 -0800

    refactor code a bit

--- lengine-compiler/src/main/scala/co/gyeongmin/lisp/compile/asmwriter/LispForWhenAsmWriter.scala
+++ lengine-compiler/src/main/scala/co/gyeongmin/lisp/compile/asmwriter/LispForWhenAsmWriter.scala
@@ -5,20 +5,31 @@ import co.gyeongmin.lisp.lexer.statements.{LispForWhenStmt, LispWhenStmt}
 import co.gyeongmin.lisp.lexer.values.symbol.LispSymbol
 import org.objectweb.asm.Label
 
+import scala.annotation.tailrec
+import scala.language.higherKinds
+
 class LispForWhenAsmWriter(v: LispForWhenStmt)(implicit runtimeEnvironment: LengineRuntimeEnvironment) {
+  @tailrec
+  private def zip3[T, U, V](t: List[T], u: List[U], v: List[V], acc: List[(T, U, V)] = Nil): List[(T, U, V)] =
+    (t, u, v) match {
+      case (Nil, Nil, Nil)                                  => acc
+      case (tHead :: tTail, uHead :: uTail, vHead :: vTail) => zip3(tTail, uTail, vTail, acc :+ (tHead, uHead, vHead))
+    }
+
   def writeValue(typeToBe: Class[_], tailRecReference: Option[(LispSymbol, Label)]): Unit = {
     val LispForWhenStmt(value, whens, otherwise) = v
-    val mv = runtimeEnvironment.methodVisitor
-    val allLabels = whens.map(_ => new Label)
-    val otherwiseLabel = new Label
-    val toLabels = allLabels.tail :+ otherwiseLabel
-    val exitLabel = new Label
+    val mv                                       = runtimeEnvironment.methodVisitor
+    val allLabels                                = whens.map(_ => new Label)
+    val otherwiseLabel                           = new Label
+    val toLabels                                 = allLabels.tail :+ otherwiseLabel
+    val exitLabel                                = new Label
 
     mv.visitLispValue(value, ObjectClass) // For target [S]
-    (whens zip (allLabels zip toLabels)) foreach {
-      case (LispWhenStmt(value, thenValue), (currentLabel, nextLabel)) =>
+
+    zip3(whens, allLabels, toLabels) foreach {
+      case (LispWhenStmt(value, thenValue), currentLabel, nextLabel) =>
         mv.visitLabel(currentLabel)
-        mv.visitDup() // [S, S]
+        mv.visitDup()                         // [S, S]
         mv.visitLispValue(value, ObjectClass) // [S, S, V]
         mv.visitStaticMethodCall(
           ObjectsClass,
@@ -26,8 +37,8 @@ class LispForWhenAsmWriter(v: LispForWhenStmt)(implicit runtimeEnvironment: Leng
           BooleanPrimitive,
           ObjectClass,
           ObjectClass
-        ) // [S, Z]
-        mv.visitIfEq(nextLabel) // [S]
+        )                                                        // [S, Z]
+        mv.visitIfEq(nextLabel)                                  // [S]
         mv.visitLispValue(thenValue, typeToBe, tailRecReference) // [S L]
         mv.visitGoto(exitLabel)
     }
