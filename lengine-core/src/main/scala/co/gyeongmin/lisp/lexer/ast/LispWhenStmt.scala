b4e2d99ae0c7a705508b025d2e97f68473c221e4
Gyeongmin Go <gyeongm@amazon.com>
2023-03-23 19:00:43 -0700

    implemented macro stuffs

--- lengine-core/src/main/scala/co/gyeongmin/lisp/lexer/ast/LispWhenStmt.scala
+++ lengine-core/src/main/scala/co/gyeongmin/lisp/lexer/ast/LispWhenStmt.scala
@@ -2,4 +2,4 @@ package co.gyeongmin.lisp.lexer.ast
 
 import co.gyeongmin.lisp.lexer.values.LispValue
 
-case class LispWhenStmt(value: LispValue, thenValue: LispValue)
+case class LispWhenStmt(value: LispValue, thenValue: LispValue) extends LispValue
