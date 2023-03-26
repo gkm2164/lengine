7f98c6b0b7bdd318a71bf66a5e0deb3db43f3b1f
Gyeongmin Go <gyeongm@amazon.com>
2023-03-25 23:50:08 -0700

    toString on tokenlists

--- lengine-core/src/main/scala/co/gyeongmin/lisp/lexer/values/LispTokenValue.scala
+++ lengine-core/src/main/scala/co/gyeongmin/lisp/lexer/values/LispTokenValue.scala
@@ -1,10 +1,15 @@
 package co.gyeongmin.lisp.lexer.values
 
 import co.gyeongmin.lisp.lexer.tokens.LispToken
+import co.gyeongmin.lisp.lexer.values.numbers.LispNumber
+import co.gyeongmin.lisp.lexer.values.seq.LispString
+import co.gyeongmin.lisp.lexer.values.symbol.VarSymbol
 
 case class LispTokenValue(lispToken: LispToken) extends LispValue {
   override def toString: String = lispToken match {
-    case v: LispValue => v.printable().getOrElse(v.toString)
+    case number: LispNumber => number.printable().getOrElse(number.toString)
+    case LispString(str) => s""""$str""""
+    case VarSymbol(name) => s"'$name'"
     case _ => s"'${lispToken.toString}'"
   }
 }
